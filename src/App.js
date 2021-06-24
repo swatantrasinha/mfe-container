96a3fe7abe92cc87c18b86ca00b3ba6ba7f490c3
Swatantra Sinha <swatantrasinha15aug@gmail.com>
2021-06-24 22:54:23 +0530

    initial commit in container

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+/*
+import MarketingApp  from './components/MarketingApp';
+import {mount} from 'marketing/MarketingApp';
+
+console.log('mount in container app : ', mount);
+*/
+
+export default () => {
+    // return (<h1>Hi There !!!</h1>)
+    return (<div>
+                <h1> Hi There !!!</h1>
+                <hr/>
+                {/* <MarketingApp /> */}
+            </div>
+            );
+};
+
